8b50454ed53b7b18bd0957f3f970b8b444ab1377
Shahar <39126169+shahar-cohen@users.noreply.github.com>
2020-05-21 04:20:10 +0300

    Update FractalDrawer.cpp

--- FractalDrawer.cpp
+++ FractalDrawer.cpp
@@ -1,5 +1,11 @@
 //
-// Created by shaharco on 02/01/2020.
+// Reads a "recipe" for a fractal from a .csv file and uses the classes in Fractal.cpp
+// to draw ascii fractals.
+// the csv file can have multiple lines containing two numbers separated by a comma, i.e. "1,2"
+// First number should be either 1, 2 or 3 denoting the type of fractal - Sierpinski Carpet, Sierpiski Triangle or Vicsek Fractal respectively.
+// Second number should be the order of the fractal (order of recursiun) - an int larger than 0 and smaller than 7.
+//
+// each line in the csv would cause the appropriate fractal to be printed to std::cout.
 //
 #include "Fractal.h"
 #include <iostream>
